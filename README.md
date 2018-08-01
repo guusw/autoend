b061ff5b86a45f1f35dd8e3795535856591728cc
guus <guus_waals@live.nl>
2018-08-01 14:06:20 +0200

    Fix switching files bug. Add ignored languages

--- README.md
+++ README.md
@@ -1,57 +1,14 @@
 # ThousandtyOne AutoEnd (A Quick Way To Place Semi Colons @ End Of Line)
 This Extension Automatically inserts the semi-colons on the right place and wraps your code to the next line the moment you type the semi-colon key on your keyboard.
 
-## Features
+----------------
 
-Quickly and automatically moves semi-colons to the end of line just by hitting the ; key on your keyboard.
-For example:
+This version has been modified to fix the following issues:
 
->**Console.WriteLine("Test";)**  
+- Switching between documents breaks the functionality
 
-**Automatically** becomes:
+This version adds the following functionality:
 
->**Console.WriteLine("Test");**
+- `ignoredLanguages` can be specified in the settings to ignore certain languages and allow them to place semicolons everywhere as usual
 
-And also automatically moves your cursor to the next line.
-
-Works best with Visual Studio C# Extension, or any extension which automatically closes bracks and quotes. So the typing experience is:
-
-> **Console.Writeline("Test;**
-
-Which Automatically Produces:
-
-> **Console.WriteLine("Test");**
-
-When the extension moves the semi-colon to the end of the line it also moves your cursor to the next line, thereby making your typing very fluid without having to manually put semi-colons explictly at the end of each line or even having to hit the enter key to move to the next line.
-
-> **Note**: Use autoend.autoInsertNewline in VS Code settings to control if autoend automically inserts a new line and moves you to the new line. Default behavior is to automtically move to new line. To prevent automatic move to new line change this setting to false. Visual Studio Code restart is required after setting change.
-
-The Extension understands context of the code in which it executes; so semi-colons will not move to end of line automatically for, 'for' or 'foreach' loops!
-
-> **Note**: For languages like Python which use colons instead of semi-colons to end the line use the autoend.autoendWithColon setting and set it to true. This disables moving of semi-colons to the end of the line and does the same thing with colons to make experience with languages like python (which use colons instead of semi-colons to end a line or indicate starting of code blocks) better. Also note that in this mode the default colon key function as is and you have to use ALT+: for the plugin to automatically move your colons to the end of line. This is done to play nice with Vim Plugins. You can of course override this behavior and map the defaut colon key to extension.colon.autoend command in keybindings.json described https://code.visualstudio.com/docs/getstarted/keybindings if you dont use Vim based Plugins.
-
-## Known Issues
-
-Please Report all issues in Github.
-
-## Release Notes
-
-First version released.
-
-### 1.0.0
-
-Initial release of Extension with basic features.
-
-### LICENSE
-
-The code is currently released under GPL-2 but if you would preffer a different license please reach out to us and we'll be happy to help you. If you would like to get involved in the project feel free to reach out to us as well.
-
-### Feedback
-
-The extension is free; but we **fully** support it and stand behind it. If you face any issues with this extension or have ideas that would improve the extension please reach out to us at contact@thousandtyone.com.
-
-(We have a feeling you'll find us responsive and willing to help.:))
-
-### Contact
-
-contact@thousandtyone.com
+Please check the [original repository](https://github.com/thousandtyone/autoend) for the full original README
--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+var ignoredLanguages: Set<string>
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -14,35 +16,37 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let editor = vscode.window.activeTextEditor;
-    let autoInsertNewline = vscode.workspace.getConfiguration('autoend').get('autoInsertNewline');
-    let autoendWithColon = vscode.workspace.getConfiguration('autoend').get('autoendWithColon');
+    let configuration = vscode.workspace.getConfiguration('autoend')
+    let autoInsertNewline = configuration.get('autoInsertNewline');
+    let autoendWithColon = configuration.get('autoendWithColon');
+    ignoredLanguages = new Set<string>();
+    configuration.get<Array<string>>('ignoredLanguages').forEach(element => {
+        ignoredLanguages.add(element);
+    });
 
-    if(autoInsertNewline == null || autoInsertNewline == undefined)
-        autoInsertNewline = true; 
+    if (autoInsertNewline == null || autoInsertNewline == undefined)
+        autoInsertNewline = true;
 
-    if(autoendWithColon == null || autoendWithColon == undefined)
+    if (autoendWithColon == null || autoendWithColon == undefined)
         autoendWithColon == false;
-        
-    if(autoendWithColon == true)
-    {
+
+    if (autoendWithColon == true) {
         let colonDisposable = vscode.commands.registerCommand('extension.colon.autoend', () => {
-          FireColonOrSemiColonCommand(editor,autoInsertNewline, ':');  
+            FireColonOrSemiColonCommand(vscode.window.activeTextEditor, autoInsertNewline, ':');
         });
         let semicolonDisposable = vscode.commands.registerCommand('extension.autoend', () => {
-          IgnoreAndPutCharacterInCurrentPostion(editor, ';')
+            IgnoreAndPutCharacterInCurrentPostion(vscode.window.activeTextEditor, ';')
         });
         context.subscriptions.push(colonDisposable);
         context.subscriptions.push(semicolonDisposable);
     }
-    else
-    {
+    else {
         let semicolonDisposable = vscode.commands.registerCommand('extension.autoend', () => {
-              FireColonOrSemiColonCommand(editor,autoInsertNewline, ';');  
-         });
-        
+            FireColonOrSemiColonCommand(vscode.window.activeTextEditor, autoInsertNewline, ';');
+        });
+
         let colonDisposable = vscode.commands.registerCommand('extension.colon.autoend', () => {
-          IgnoreAndPutCharacterInCurrentPostion(editor, ':')
+            IgnoreAndPutCharacterInCurrentPostion(vscode.window.activeTextEditor, ':')
         });
         context.subscriptions.push(colonDisposable);
         context.subscriptions.push(semicolonDisposable);
@@ -50,49 +54,52 @@ export function activate(context: vscode.ExtensionContext) {
     }
 }
 
-function FireColonOrSemiColonCommand(editor, autoInsertNewline, character)
-{
-            let lineNumber : number = editor.selection.active.line;
-            let columnNumber : number = editor.selection.active.character;
-            let lineText = editor.document.lineAt(lineNumber).text;
-            let lineLength = lineText.length;
-            let trimmedText = lineText.replace(/\s+/g, " ").trim();                   
-            if(OkToPutSemiColonInCurrentPosition(trimmedText, lineNumber, columnNumber))
-            {
-                IgnoreAndPutCharacterInCurrentPostion(editor, character);
-            }
-            else
-            {
-                editor.edit((textEditor)=>{       
-                    textEditor.insert(new vscode.Position(lineNumber,lineLength), character);
-                })
-                
-                if(autoInsertNewline)
-                    vscode.commands.executeCommand('editor.action.insertLineAfter');
-                else
-                    vscode.commands.executeCommand('cursorEnd');
-            }    
+function FireColonOrSemiColonCommand(editor, autoInsertNewline, character) {
+    let document: vscode.TextDocument = editor.document
+
+    // Check ignores
+    if (ignoredLanguages.has(document.languageId)) {
+        IgnoreAndPutCharacterInCurrentPostion(editor, character);
+        return;
+    }
+
+    let lineNumber: number = editor.selection.active.line;
+    let columnNumber: number = editor.selection.active.character;
+    let lineText = editor.document.lineAt(lineNumber).text;
+    let lineLength = lineText.length;;
+    let trimmedText = lineText.replace(/\s+/g, " ").trim();
+    if (OkToPutSemiColonInCurrentPosition(trimmedText, lineNumber, columnNumber)) {
+        IgnoreAndPutCharacterInCurrentPostion(editor, character);
+    }
+    else {
+        editor.edit((textEditor) => {
+            textEditor.insert(new vscode.Position(lineNumber, lineLength), character);
+        })
+
+        if (autoInsertNewline)
+            vscode.commands.executeCommand('editor.action.insertLineAfter');
+        else
+            vscode.commands.executeCommand('cursorEnd');
+    }
 }
 
-function IgnoreAndPutCharacterInCurrentPostion(editor, character)
-{
-    let lineNumber : number = editor.selection.active.line;
-                    let columnNumber : number = editor.selection.active.character;
-                    let lineText = editor.document.lineAt(lineNumber).text;
-                    let lineLength = lineText.length;
-                    let trimmedText = lineText.replace(/\s+/g, " ").trim();  
-    editor.edit((textEditor)=>{       
-                            textEditor.insert(new vscode.Position(lineNumber,columnNumber), character);
-                        })
+function IgnoreAndPutCharacterInCurrentPostion(editor, character) {
+    let lineNumber: number = editor.selection.active.line;
+    let columnNumber: number = editor.selection.active.character;
+    let lineText = editor.document.lineAt(lineNumber).text;
+    let lineLength = lineText.length;
+    let trimmedText = lineText.replace(/\s+/g, " ").trim();
+    editor.edit((textEditor) => {
+        textEditor.insert(new vscode.Position(lineNumber, columnNumber), character);
+    })
 }
 
-function OkToPutSemiColonInCurrentPosition(trimmedText, lineNumber, columnNumber)
-{
-  var textIsALoop = trimmedText.startsWith('for(') | trimmedText.startsWith('foreach(')
-                    | trimmedText.startsWith('for (') | trimmedText.startsWith('foreach (');
-  var textIsAComment = trimmedText.startsWith("//")
-  var alreadyEndsWithSemiColon = trimmedText.endsWith(';')
-  return textIsALoop || textIsAComment || alreadyEndsWithSemiColon;
+function OkToPutSemiColonInCurrentPosition(trimmedText, lineNumber, columnNumber) {
+    var textIsALoop = trimmedText.startsWith('for(') | trimmedText.startsWith('foreach(')
+        | trimmedText.startsWith('for (') | trimmedText.startsWith('foreach (');
+    var textIsAComment = trimmedText.startsWith("//")
+    var alreadyEndsWithSemiColon = trimmedText.endsWith(';')
+    return textIsALoop || textIsAComment || alreadyEndsWithSemiColon;
 }
 
 // this method is called when your extension is deactivated
